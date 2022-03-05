c8eb0dc3a8934637a51206144f690855631ddaab
jun <qurugi0347@gmail.com>
2022-03-06 01:48:28 +0900

    Fix: api userSearch 오류 수정

--- api/src/feature/user/user.service.ts
+++ api/src/feature/user/user.service.ts
@@ -18,20 +18,22 @@ export class UserService {
   ): Promise<IPaginationResult<User>> {
     const userSelectQuery = this.userRepository
       .makeQueryBuilder()
-      .select(User.summaryData("user"))
-      .andWhere(
+      .select(User.summaryData("user"));
+    if (search.search) {
+      userSelectQuery.andWhere(
         `
-        (
-          user.name like :search
-          or user.email like :search
-          or user.phone like :phoneSearch
-        )
-      `,
+          (
+            user.name like :search
+            or user.email like :search
+            or user.phone like :phoneSearch
+          )
+        `,
         {
           search: `%${search.search}%`,
           phoneSearch: `%${search.search.replace(/-/, "")}%`,
         }
       );
+    }
     const total = await userSelectQuery.getCount();
     const userData = await userSelectQuery
       .offset(pagination.limit * (pagination.page - 1))
